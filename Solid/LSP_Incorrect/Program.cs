76b55ca54788b4ef2f9a1da46391615714604b82
Chander Dhall <csdhall@gmail.com>
2013-07-26 22:22:11 -0700

    Added LSP

---
+++ Solid/LSP_Incorrect/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP_Incorrect
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public interface Shape
+        {
+            int Area();
+        }
+
+        public class Rectangle : Shape
+        {
+            public virtual int Height { get; set; }
+            public virtual int Width { get; set; }
+
+            public int Area() { return Height * Width; }
+        }
+
+        public class Square : Shape
+        {
+            public int LengthOfSides { get; set; }
+
+            public int Area() { return LengthOfSides ^ 2; }
+        }
+
+        public class DoStuff
+        {
+            public void DoTheRectangleStuff()
+            {
+                Rectangle rectangle = new Rectangle();
+                rectangle.Height = 4;
+                rectangle.Width = 5;
+                AssertTheArea(rectangle, 20);
+            }
+
+            public void DoTheSquareStuff()
+            {
+                Square square = new Square();
+                square.LengthOfSides = 4;
+                AssertTheArea(square, 16);
+            }
+
+            private void AssertTheArea(Shape shape, int expectedArea)
+            {
+                int actualArea = shape.Area();
+                Debug.Assert(expectedArea == actualArea);
+            }
+        }
+    }
+}
---
+++ Solid/LSP_Wrong/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LSP_Wrong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Height = 4;
+            rectangle.Width = 5;
+            AssertTheArea(rectangle);
+
+            Rectangle square = new Square();
+            square.Height = 5;
+            AssertTheArea(square);
+
+        }
+
+        public class Rectangle
+        {
+            public virtual int Height { get; set; }
+            public virtual int Width { get; set; }
+
+            public int Area()
+            {
+                return Height * Width;
+            }
+        }
+
+        public class Square : Rectangle
+        {
+            public override int Height
+            {
+                get { return base.Height; }
+                set
+                {
+                    base.Height = value;
+                    base.Width = value;
+                }
+            }
+            public override int Width
+            {
+                get { return base.Width; }
+                set
+                {
+                    base.Width = value;
+                    base.Height = value;
+                }
+            }
+        }
+
+        private static void AssertTheArea(Rectangle rectangle)
+        {
+            int expectedArea = 20;
+            int actualArea = rectangle.Area();
+            Debug.Assert(expectedArea == actualArea);
+        }
+
+
+    }
+}
